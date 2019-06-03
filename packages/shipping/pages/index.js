60ae11a8eabb63eff817e6161e5c0fc02678175d
chanaru <19930305Sam>
2019-06-03 16:27:25 +0530

    first commit

---
+++ packages/shipping/pages/index.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import IncrementButton from '@sprii/common';
+
+export default () => {
+  // Declare a new state variable, which we'll call "count"
+
+  return (
+    <div>
+      <IncrementButton text='HOME/SHIPPING/INDEX You clicked' />
+    </div>
+  );
+};
